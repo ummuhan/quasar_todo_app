615395da99d2b2389b6ccc98fdcd48a574b3d6e6
Ummuhan <ummuhanoksuz1@gmail.com>
2020-12-21 18:45:59 +0300

    New changes.(Card added.)

--- src/pages/Help.vue
+++ src/pages/Help.vue
@@ -1,9 +1,65 @@
 <template>
-  <q-page class="q-pa-lg">
-    <h5 class="q-mt-none">Help</h5>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem vero consectetur, cupiditate dolorem vitae quod quisquam quo sint reprehenderit, officiis ut consequatur nulla harum, facere iste itaque repellat rerum deleniti fugiat aliquid nisi necessitatibus. Doloribus, exercitationem enim eos itaque at nisi laudantium unde placeat, perspiciatis eveniet libero ab, voluptatem nulla?</p>
-    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cum, minus obcaecati. Tempora dolorem quae dolor!</p>
-  </q-page>
+  <div class="q-pa-md row items-start q-gutter-md">
+    <q-card class="my-card">
+      <img src="https://cdn.pixabay.com/photo/2017/03/09/12/31/error-2129569_1280.jpg">
+
+      <q-card-section>
+        <div class="text-h6">Our Changing Planet</div>
+        <div class="text-subtitle2">by John Doe</div>
+      </q-card-section>
+
+      <q-card-section class="q-pt-none">
+       Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis sit, commodi distinctio expedita nostrum provident neque nulla ex voluptatum! Facere, quas quibusdam. Odio blanditiis quo minus error magnam eligendi animi?
+      </q-card-section>
+    </q-card>
+
+    <q-card class="my-card">
+      <q-img
+        src="https://cdn.quasar.dev/img/parallax2.jpg"
+        basic
+      >
+        <div class="absolute-bottom text-subtitle2 text-center">
+          Title
+        </div>
+      </q-img>
+    </q-card>
+
+    <q-card class="my-card">
+      <q-img
+        src="https://cdn.quasar.dev/img/parallax2.jpg"
+        basic
+      >
+        <div class="absolute-bottom text-h6">
+          Title
+        </div>
+      </q-img>
+
+      <q-card-section>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptate earum consequuntur modi. Sequi reiciendis quaerat dolore consectetur temporibus ipsum voluptates optio suscipit molestiae. Quisquam, iure. Amet eos corporis exercitationem ab.
+      </q-card-section>
+    </q-card>
+
+    <q-card class="my-card">
+      <q-img src="https://cdn.quasar.dev/img/parallax2.jpg">
+        <div class="text-subtitle2 absolute-top text-center">
+          Title
+        </div>
+      </q-img>
+    </q-card>
+
+    <q-card class="my-card">
+      <q-img src="https://cdn.quasar.dev/img/parallax2.jpg">
+        <div class="text-h5 absolute-bottom text-right">
+          Title
+        </div>
+      </q-img>
+    </q-card>
+
+    
+
+    
+ 
+  </div>
 </template>
 
 <script>
--- src/pages/Settings.vue
+++ src/pages/Settings.vue
@@ -1,11 +1,35 @@
 <template>
-  <q-page class="q-pa-lg">
-   <h5 class="q-mt-none">Settings</h5>
-  </q-page>
+  <div class="q-pa-md row items-start q-gutter-md">
+    <q-card dark bordered class="bg-grey-7 my-card">
+      <q-card-section>
+        <div class="text-h6">Our Changing Planet</div>
+        <div class="text-subtitle2">by John Doe</div>
+      </q-card-section>
+
+      <q-separator dark inset />
+
+      <q-card-section>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias mollitia
+        sunt, sit reprehenderit possimus accusamus quae id beatae, adipisci
+        ducimus, consequatur praesentium quia sapiente dolore delectus nesciunt
+        soluta aliquid quam.
+      </q-card-section>
+      <q-card-section>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias mollitia
+        sunt, sit reprehenderit possimus accusamus quae id beatae, adipisci
+        ducimus, consequatur praesentium quia sapiente dolore delectus nesciunt
+        soluta aliquid quam. Lorem ipsum dolor sit, amet consectetur adipisicing
+        elit. Corrupti laborum labore aperiam repellat quaerat exercitationem
+        amet et sed maiores consectetur quidem minima necessitatibus debitis,
+        quis, ab voluptas magnam aspernatur deserunt officia fugiat iure
+        sapiente doloribus dignissimos voluptatem. Nesciunt cum dolore magnam,
+        voluptas aspernatur, iusto excepturi unde nam esse molestiae labore.
+      </q-card-section>
+    </q-card>
+  </div>
 </template>
 
 <script>
-export default {
- 
-}
+export default {};
 </script>
+
